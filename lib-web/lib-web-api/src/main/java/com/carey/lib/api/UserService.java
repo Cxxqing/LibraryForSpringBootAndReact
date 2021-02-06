2107aaad036df851cf2c6a4d776964c27146d9ef
Carey <2474961332@qq.com>
2021-02-06 10:17:09 +0800

    feat version1.0 init commint

---
+++ lib-web/lib-web-api/src/main/java/com/carey/lib/api/UserService.java
@@ -0,0 +1,14 @@
+package com.carey.lib.api;
+
+
+import com.carey.lib.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
